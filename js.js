0ae08fccfc2817e0d3642f4ccedf9dd13803139b
karlsdcm <175364597+karusodcm@users.noreply.github.com>
2025-12-14 14:47:08 -0500

    update script

--- js.js
+++ js.js
@@ -1,2 +1,16 @@
 const scroll = () => innerWidth - document.documentElement.clientWidth
 document.documentElement.style.setProperty('--scroll-bar', scroll())
+
+
+const btnleft = document.querySelector("#left")
+const btnright = document.querySelector("#right")
+const carrusel = document.querySelector("#mcarrusel")
+const sliderselection = document.querySelectorAll(".midis-all-section")
+
+function moveToRight(){
+
+}
+
+function moveToLeft(){
+    
+}
